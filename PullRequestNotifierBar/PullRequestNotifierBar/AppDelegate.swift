8f2d4f9f2d5670a59d9520c87e9fc8d6c79b38a1
Nicolas Battelli <nbattelli@gmail.com>
2016-01-12 22:25:42 -0300

    Creo un menu con la opción quit y lo muestro en el Status Bar

--- PullRequestNotifierBar/PullRequestNotifierBar/AppDelegate.swift
+++ PullRequestNotifierBar/PullRequestNotifierBar/AppDelegate.swift
@@ -11,11 +11,18 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
 
 
+    @IBAction func quitClicked(sender: NSMenuItem) {
+        NSApplication.sharedApplication().terminate(self)
+    }
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        statusItem.title = "PR Notifier"
+        statusItem.menu = statusMenu
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
